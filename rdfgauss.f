b1a0d448d27b938f1e0207ed0e96eff4d82f944d
tomoya <tomoya05242003@icloud.com>
2025-01-25 22:41:17 +0900

    元素種ペアを選択できるように変更

--- rdfgauss.f
+++ rdfgauss.f
@@ -1,31 +1,36 @@
       program rdfgauss
       implicit none
 c----- Variable decreation 
-      integer natm,natmo, mx, my, mz, l
-      integer ia, ja, k, elemIndex, ix, iy, iz, i
+      integer natm,natmo, mx, my, mz, l,own,pair
+      integer ia, ja, k, elemIndex, ix, iy, iz, i,ioa,ipa
       integer, allocatable::zz(:), elemType(:), elemCounts(:)
       real*8 hunit, dd, omega, r_max
       real*8 rho, dx, dy, dz, dis, sigma, gauss, dt, w, nfactor
       real*8 hm(3, 3)
       real*8, allocatable:: ta(:, :), sa(:, :)
       real*8, allocatable :: uco(:,:), co(:,:), rdf(:)
-      real*8, parameter ::pi =  3.141592d0 
-      
+      real*8, allocatable :: ownCo(:,:),pairCo(:,:)
+      real*8, parameter ::pi =  3.141592d0  
+      character(len=30) filePath
 
 c-----file read
-      read(*, *)
-      read(*, *)
-      read(*, *) hm(1, 1), hm(2, 1), hm(3, 1)
-      read(*, *) hm(1, 2), hm(2, 2), hm(3, 2)
-      read(*, *) hm(1, 3), hm(2, 3), hm(3, 3)
-      read(*, *)
-      read(*, *) natmo
+c-----HACK:file pathを標準入力、実行時に入力して、書き直しをなくしたい
+      open(13, file='../a-SiO2.xsf', status='old')
+      read(13, *)
+      read(13, *)
+      read(13, *) hm(1, 1), hm(2, 1), hm(3, 1)
+      read(13, *) hm(1, 2), hm(2, 2), hm(3, 2)
+      read(13, *) hm(1, 3), hm(2, 3), hm(3, 3)
+      read(13, *)
+      read(13, *) natmo
 
       allocate(sa(3, natmo), zz(natmo),
      &         ta(3, natmo), elemType(natmo), co(3,natmo))
       do ia =  1, natmo
-        read(*, *) zz(ia), sa(1, ia), sa(2, ia), sa(3, ia)
+        read(13, *) zz(ia), sa(1, ia), sa(2, ia), sa(3, ia)
       end do
+      close(13)
+
 
 c-----count. the number of type of atom
       elemType(1) = 1
@@ -50,13 +55,33 @@ c-----count. the number of each atoms
         end do
       end do
 
-c-----define co
-      do ia =  1, natmo
-        do i =  1, 3
-          co(i, ia) = sa(i, ia)
-        end do
-      end do
+c-----input own atom and pair atom number
+      write(6, *) "Input own atom , pair atom"
+      write(6, *) "Enter a less than or equal to ", elemType(natmo)
+      read(5, *) own, pair
+      allocate(ownCo(3,elemCounts(own)),pairCo(3,(elemCounts(pair))))
 
+
+c-----define co
+      ownCo(:,:) = 0
+      pairCo(:,:) = 0
+      ioa = 0
+      ipa = 0
+
+      do ia =  1, natmo
+        if (elemType(ia).eq.own) then
+          ioa = ioa + 1
+          do i =  1, 3
+            ownCo(i, ioa) = sa(i, ia)
+          end do
+        end if
+        if (elemType(ia).eq.pair) then
+          ipa = ipa + 1
+          do i =  1, 3
+            pairCo(i,ipa) = sa(i,ia)
+          end do
+        end if
+      end do
 
 c-----setting pairs
       r_max = 10.0d0
@@ -64,7 +89,7 @@ c-----setting pairs
       my = aint(r_max / hm(2,2)) + 1
       mz = aint(r_max / hm(3,3)) + 1
 
-      natm = natmo*((2*mx + 1)*(2*my + 1)*(2*mz + 1))
+      natm = elemCounts(pair)*((2*mx + 1)*(2*my + 1)*(2*mz + 1))
       allocate(uco(3,natm))
 
       uco(:,:) = 0.0d0
@@ -74,17 +99,17 @@ c-----setting pairs
       do iy =  -my, my
       do iz =  -mz, mz
 c    ia max is natm or natmo ?
-        do ia =  1, natmo
+        do ia =  1, elemCounts(pair)
           ja = ja + 1
-          uco(1,ja) = co(1,ia)+hm(1,1)*dble(ix)
-     &                        +hm(1,2)*dble(iy)
-     &                        +hm(1,3)*dble(iz)
-          uco(2,ja) = co(1,ia)+hm(2,1)*dble(ix)
-     &                        +hm(2,2)*dble(iy)
-     &                        +hm(2,3)*dble(iz)
-          uco(3,ja) = co(1,ia)+hm(3,1)*dble(ix)
-     &                        +hm(3,2)*dble(iy)
-     &                        +hm(3,3)*dble(iz)
+          uco(1,ja) = pairCo(1,ia)+hm(1,1)*dble(ix)
+     &                            +hm(1,2)*dble(iy)
+     &                            +hm(1,3)*dble(iz)
+          uco(2,ja) = pairCo(1,ia)+hm(2,1)*dble(ix)
+     &                            +hm(2,2)*dble(iy)
+     &                            +hm(2,3)*dble(iz)
+          uco(3,ja) = pairCo(1,ia)+hm(3,1)*dble(ix)
+     &                            +hm(3,2)*dble(iy)
+     &                            +hm(3,3)*dble(iz)
         end do
       end do        
       end do
@@ -98,39 +123,38 @@ c------calc.distance from own atom to pair atom
       rho = dble(natmo) / omega
 c     l, sigma, w:temp
       l = 100
-      sigma = 0.5d0
+      sigma = 0.2d0
       w = r_max / dble(l)
       allocate(rdf(l))
 
-      do ia =  1, natmo
+      do ia =  1, elemCounts(own)
         do ja =  1, natm
-          dx = uco(1,ja) - co(1,ia)
-          dy = uco(2,ja) - co(2,ia)
-          dz = uco(3,ja) - co(3,ia)
+          dx = uco(1,ja) - ownCo(1,ia)
+          dy = uco(2,ja) - ownCo(2,ia)
+          dz = uco(3,ja) - ownCo(3,ia)
           dis = sqrt(dx**2 + dy**2 + dz**2)
 
           if (dis.lt.1.0d-8.or.dis.gt.l*1.5d0) cycle
-          nfactor = 4.0d0*pi*rho*dble(natmo)*(dis)**2
+c-----TODO:規格化因子が正しいか確認
+          nfactor = 4.0d0*pi*rho*dble(elemCounts(pair))*(dis)**2
 c         w:mesh width, (l-1)*w = r_max
           do k =  1, l
             dt = (k-1)*w
             gauss = dexp(-0.5d0* ((dt-dis)/sigma)**2) 
      &        / (sigma * dsqrt(2.0d0*pi))
             rdf(k) = rdf(k) + (gauss / nfactor)
-
-c            if (ja.eq.natm.and.ia.eq.2) then
-c              write(*, *) "gauss",gauss / nfactor, "dt", dt
-c            end if
           end do
         end do
       end do
 
 
-c-----TODO:file write
-      open(11, file='rdf.dat', status='replace')
+c-----file write
+c-----TODO:元素ペアが選択されたらファイル名をコード内で決めて標準入力部分をなくす
+      write(6, *) "Input filepath to write data"
+      read(*, '(A)') filePath
+      open(11, file=filePath, status='replace')
       do k =  1, l
         write(11, *) w*(k-1), rdf(k)
-c        write(*, *) w*(k-1), rdf(k)
       end do
       close(11)
 
--- read.py
+++ read.py
@@ -1,17 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-file_path = "rdf.dat"
+def read_file(filePath):
+  data = np.loadtxt(filePath)
+  x = data[:, 0]  # 1列目
+  y = data[:, 1]  # 2列目
+  return x, y
 
-data = np.loadtxt(file_path)
 
-x = data[:, 0]  # 1列目
-y = data[:, 1]  # 2列目
+filePath = ["si-si.dat","si-o.dat","o-o.dat"]
 
-plt.figure(figsize=(8, 6))
-plt.plot(x, y, label="Data", marker="o")
+plt.figure(figsize=(10,6))
+x_data = []
+y_data = []
 
-plt.title("Data Visualization")
+for path in filePath:
+  x ,y = read_file(path)
+  x_data.append(x)
+  y_data.append(y)
+  
+plt.plot(x_data[0],y_data[0],label=f"Si-Si",color="blue")
+plt.plot(x_data[1],y_data[1],label=f"Si-O",color="green")
+plt.plot(x_data[2],y_data[2],label=f"O-O",color="red")
+
+
+
+plt.title("Visualization of RDF")
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.legend()
